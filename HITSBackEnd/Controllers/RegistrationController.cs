69c1f3b87a9bbf32fe92cc0829a5666a004b1715
Artyom Bauer <artem_bauer@inbox.ru>
2023-10-19 23:30:36 +0700

    Добавлен контроллер для обработки запросов на регистрацию.Добавлено занесения пользователя в базу данных. Добавлена валидация номера телефона и проверка на существование пользователя

---
+++ HITSBackEnd/Controllers/RegistrationController.cs
@@ -0,0 +1,27 @@
+﻿using HITSBackEnd.baseClasses;
+using HITSBackEnd.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HITSBackEnd.Controllers
+{
+    [ApiController]
+    [Route("api/account")]
+    public class RegistrationController : ControllerBase
+    {
+        private readonly Registration _userRegistration;
+
+        public RegistrationController(Registration userRegistration)
+        {
+            _userRegistration = userRegistration;
+        }
+        [HttpPost("register")]
+        public IActionResult Register([FromBody] User request)
+        {
+        _userRegistration.registrateUser(request);
+
+        return Ok("Registration successful");
+
+        }
+    }
+}
